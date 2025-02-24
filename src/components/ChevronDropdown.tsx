99be741917cb71a3ad48498e31e9556612404abc
Miruzen <114496215+Miruzen@users.noreply.github.com>
2025-02-24 20:27:22 +0700

    adjusting

--- src/components/ChevronDropdown.tsx
+++ src/components/ChevronDropdown.tsx
@@ -23,7 +23,7 @@ const ChevronDropdown = ({ options }) => {
 
       {/* Dropdown Menu */}
       {isOpen && (
-        <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg z-50">
+        <div className="absolute right-5 mt-2 w-48 bg-white rounded-md shadow-lg z-50">
           <div className="py-1">
             {options.map((option, index) => (
               <button
