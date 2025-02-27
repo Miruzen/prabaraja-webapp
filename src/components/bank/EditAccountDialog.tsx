233fc3d6c44f79010a6090c2cbd4a37b6d73e451
Miruzen <114496215+Miruzen@users.noreply.github.com>
2025-02-27 14:09:41 +0700

    Bug text

--- src/components/bank/EditAccountDialog.tsx
+++ src/components/bank/EditAccountDialog.tsx
@@ -89,7 +89,7 @@ export function EditAccountDialog({ open, onOpenChange, account, onSave }: EditA
                   <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
                 </Button>
               </PopoverTrigger>
-              <PopoverContent className="w-[400px] p-0 bg-white">
+              <PopoverContent className="w-[400px] p-0 bg-black">
                 <Command>
                   <CommandInput 
                     placeholder="Search bank..."
