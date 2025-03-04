9f847d0732577838c99f3556a7dbacf2509f8780
Miruzen <114496215+Miruzen@users.noreply.github.com>
2025-03-04 13:52:08 +0700

    Text color

--- src/pages/Purchases.tsx
+++ src/pages/Purchases.tsx
@@ -209,19 +209,19 @@ const Purchases = () => {
                 </DropdownMenuTrigger>
                 <DropdownMenuContent align="end" className="bg-white">
                   <DropdownMenuItem onClick={() => navigateToCreatePurchase("invoice")}>
-                    <FileText className="mr-2 h-4 w-4" /> New Invoice
+                    <FileText className="mr-2 h-4 w-4 text-purple-500" /> New Invoice
                   </DropdownMenuItem>
                   <DropdownMenuItem onClick={() => navigateToCreatePurchase("shipment")}>
-                    <Package className="mr-2 h-4 w-4" /> New Shipment
+                    <Package className="mr-2 h-4 w-4 text-orange-500" /> New Shipment
                   </DropdownMenuItem>
                   <DropdownMenuItem onClick={() => navigateToCreatePurchase("order")}>
-                    <ShoppingCart className="mr-2 h-4 w-4" /> New Order
+                    <ShoppingCart className="mr-2 h-4 w-4 text-blue-500" /> New Order
                   </DropdownMenuItem>
                   <DropdownMenuItem onClick={() => navigateToCreatePurchase("offer")}>
-                    <Tag className="mr-2 h-4 w-4" /> New Offer
+                    <Tag className="mr-2 h-4 w-4 text-green-500" /> New Offer
                   </DropdownMenuItem>
                   <DropdownMenuItem onClick={() => navigateToCreatePurchase("request")}>
-                    <FileQuestion className="mr-2 h-4 w-4" /> New Request
+                    <FileQuestion className="mr-2 h-4 w-4 text-pink-500" /> New Request
                   </DropdownMenuItem>
                 </DropdownMenuContent>
               </DropdownMenu>
